e12316b36e514cc1f11a27f9101c86d8ac1f204d
debayansamal <debayansamal@gmail.com>
2026-08-02 10:26:56 +0530

    docs(examples): add Windows path separator normalization example

---
+++ examples/windows_paths.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/debayansamal/port-mortem-picomatch-go"
+)
+
+func main() {
+	// Windows path separator normalization example
+	opts := &picomatch.Options{Windows: true}
+	isMatch, _ := picomatch.IsMatch("src\\components\\Button.tsx", "src/*/*.tsx", opts)
+	fmt.Printf("IsMatch('src\\\\components\\\\Button.tsx', 'src/*/*.tsx', Windows) = %v\n", isMatch)
+}
