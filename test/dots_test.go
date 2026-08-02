7d794b67e60b1786f02871ec0c0bb989be9c68ac
debayansamal <debayansamal@gmail.com>
2026-08-02 19:23:22 +0530

    test(features): add dedicated test suite for dotfile handling

---
+++ test/dots_test.go
@@ -0,0 +1,33 @@
+package test
+
+import (
+	"testing"
+	"github.com/debayansamal/port-mortem-picomatch-go"
+)
+
+func TestDotfilePatterns(t *testing.T) {
+	optsWithDot := &picomatch.Options{Dot: true}
+	optsNoDot := &picomatch.Options{Dot: false}
+
+	tests := []struct {
+		pattern string
+		input   string
+		opts    *picomatch.Options
+		want    bool
+	}{
+		{"*.js", ".foo.js", optsNoDot, false},
+		{"*.js", ".foo.js", optsWithDot, true},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.pattern+"_"+tc.input, func(t *testing.T) {
+			got, err := picomatch.IsMatch(tc.input, tc.pattern, tc.opts)
+			if err != nil {
+				t.Fatalf("IsMatch error: %v", err)
+			}
+			if got != tc.want {
+				t.Errorf("IsMatch(%q, %q) = %v, want %v", tc.input, tc.pattern, got, tc.want)
+			}
+		})
+	}
+}
